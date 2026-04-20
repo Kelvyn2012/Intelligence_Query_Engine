3d7fc68b0ad7876264d6d2aa30d80442b7768b40
kelvyn2012 <kudufo@gmail.com>
2026-04-20 17:27:19 +0100

    added thrailing slash to my urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from .views import ProfileDetailView, ProfileSearchView, ProfileView
 
 urlpatterns = [
-    # Search must be declared before <uuid:id> to avoid ambiguity
-    path("profiles/search", ProfileSearchView.as_view(), name="profiles-search"),
-    path("profiles", ProfileView.as_view(), name="profiles"),
-    path("profiles/<uuid:id>", ProfileDetailView.as_view(), name="profile-detail"),
+    path("profiles/search/", ProfileSearchView.as_view(), name="profiles-search"),
+    path("profiles/", ProfileView.as_view(), name="profiles"),
+    path("profiles/<uuid:id>/", ProfileDetailView.as_view(), name="profile-detail"),
 ]
